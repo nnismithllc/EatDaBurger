a652e94d4690d30a1ebdfe28e45914dfc02a6853
nnismithllc <65423204+nnismithllc@users.noreply.github.com>
2020-09-12 23:32:06 -0400

    Added files and updates schema files

---
+++ db/seeds.sql
@@ -0,0 +1,6 @@
+INSERT INTO burger (name) VALUES ('Twinkle Onion Swiss');
+INSERT INTO burger (name) VALUES ('Mushroom Fried Sunlight');
+INSERT INTO burger (name, devour) VALUES ('Rain Veggie', true);
+INSERT INTO burger (name, devour) VALUES ('Bacon Freeze', true);
+INSERT INTO burger (name, devour) VALUES ('Delite Lolipop Pepper', true);
+INSERT INTO burger (name) VALUES ('Orange Peach Twist');
