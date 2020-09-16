f90780a2cab9cb2554649e65a6c01a16e7a96a83
nnismithllc <65423204+nnismithllc@users.noreply.github.com>
2020-09-16 00:38:59 -0400

    updated all files and install npm dependencies

--- db/schema.sql
+++ db/schema.sql
@@ -1,9 +1,10 @@
 ### Schema
 
+DROP DATABASE IF EXISTS burger_db;
 CREATE DATABASE burger_db;
 USE burger_db;
 
-CREATE TABLE burger
+CREATE TABLE burgers
 (
 	id int NOT NULL AUTO_INCREMENT,
 	name varchar(255) NOT NULL,
--- db/seeds.sql
+++ db/seeds.sql
@@ -1,6 +1,6 @@
-INSERT INTO burger (name) VALUES ('Twinkle Onion Swiss');
-INSERT INTO burger (name) VALUES ('Mushroom Fried Sunlight');
-INSERT INTO burger (name, devour) VALUES ('Rain Veggie', true);
-INSERT INTO burger (name, devour) VALUES ('Bacon Freeze', true);
-INSERT INTO burger (name, devour) VALUES ('Delite Lolipop Pepper', true);
-INSERT INTO burger (name) VALUES ('Orange Peach Twist');
+INSERT INTO burgers (name) VALUES ('Twinkle Onion Swiss');
+INSERT INTO burgers (name) VALUES ('Mushroom Fried Sunlight');
+INSERT INTO burgers (name, devour) VALUES ('Rain Veggie', true);
+INSERT INTO burgers (name, devour) VALUES ('Bacon Freeze', true);
+INSERT INTO burgers (name, devour) VALUES ('Delite Lollipop Pepper', true);
+INSERT INTO burgers (name) VALUES ('Orange Peach Twist');
