36bfc21134a671dc28b2a37a61ed9b7fcce5a90c
nnismithllc <65423204+nnismithllc@users.noreply.github.com>
2020-10-03 00:00:01 -0400

    added css colors to files and aligned text

--- server.js
+++ server.js
@@ -1,10 +1,11 @@
+// Require Express Module and Port Information
 var express = require("express");
-
 var PORT = process.env.PORT || 8080;
 var app = express();
 
 // Serve static content for the app from the "public" directory in the application directory.
 app.use(express.static("public"));
+app.use(express.static('public/images')); 
 
 // Parse application body as JSON
 app.use(express.urlencoded({ extended: true }));
@@ -23,7 +24,8 @@ app.use(routes);
 
 // Start our server so that it can begin listening to client requests.
 app.listen(PORT, function() {
-  // Log (server-side) when our server has started
+
+// Log (server-side) when our server has started
   console.log("Server listening on: http://localhost:" + PORT);
 });
 
